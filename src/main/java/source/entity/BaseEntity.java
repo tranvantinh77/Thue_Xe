6f489da34106e78a260d6f6e1a2c15c03f546be0
Van Tinh <67227974+tranvantinh117@users.noreply.github.com>
2021-07-25 20:46:41 +0700

    JpaAuditing

--- src/main/java/source/entity/BaseEntity.java
+++ src/main/java/source/entity/BaseEntity.java
@@ -1,9 +1,16 @@
 package source.entity;
 
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import javax.persistence.*;
 import java.sql.Date;
 
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
 
     @Id
@@ -11,15 +18,19 @@ public class BaseEntity {
     private Long id;
 
     @Column
+    @CreatedBy
     private String createBy;
 
     @Column
+    @CreatedDate
     private Date createDate;
 
     @Column
+    @LastModifiedBy
     private String updateBy;
 
     @Column
+    @LastModifiedDate
     private Date updateDate;
 
     @Column
