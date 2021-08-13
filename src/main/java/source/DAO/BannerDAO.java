9e81c18aed290758a3b1d2694dd787df7c9bdef0
trnmnhkhanh <trnmnhkhanh99@gmail.com>
2021-08-13 23:24:49 +0700

    update banner

--- src/main/java/source/DAO/BannerDAO.java
+++ src/main/java/source/DAO/BannerDAO.java
@@ -21,9 +21,9 @@ public interface BannerDAO extends JpaRepository<BannerEntity, Long> {
     @Query(value = "select * from banner where active_fag = 0", nativeQuery = true)
     List<BannerEntity> findByActiveFag();
 
-    @Query(value = "select * from banner carCom where (carCom.active_fag=0) and (carCom.code like %:keyword% or carCom.name like %:keyword% )",nativeQuery = true)
+    @Query(value = "select * from banner banner where (banner.active_fag=0) and (banner.code like %:keyword% or banner.name like %:keyword% )",nativeQuery = true)
     List<BannerEntity> findByKeyword(@Param("keyword") String keyword, Pageable pageable);
 
-    @Query(value = "select * from banner carCom where (carCom.active_fag=0) and (carCom.code like %:keyword% or carCom.name like %:keyword% )",nativeQuery = true)
+    @Query(value = "select * from banner banner where (banner.active_fag=0) and (banner.code like %:keyword% or banner.name like %:keyword% )",nativeQuery = true)
     List<BannerEntity> findByKeyword(@Param("keyword") String keyword);
 }
